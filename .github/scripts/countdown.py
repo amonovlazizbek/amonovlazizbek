914852199f45c71983a4c0e594fa3076809c5a0b
Lazizbek <amonovlazizbek9@gmail.com>
2026-04-16 20:57:41 +0500

    update github workflows and scripts

---
+++ .github/scripts/countdown.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+target = datetime(2029, 5, 25, 0, 0, 0)
+now = datetime.utcnow()
+
+diff = target - now
+
+days = diff.days
+hours, rem = divmod(diff.seconds, 3600)
+minutes, seconds = divmod(rem, 60)
+
+output = f"""
+⏳ Time Remaining to Graduation:
+📅 {days} days
+⏰ {hours} hours
+🕒 {minutes} minutes
+⏱ {seconds} seconds
+"""
+
+with open("countdown.txt", "w", encoding="utf-8") as f:
+    f.write(output)
